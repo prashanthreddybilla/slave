81cf439cfc51514321e7330edd29ff105f85fb13
Prashanth Reddy Billa <prashanth.billa@valuelabs.com>
2021-12-08 12:33:52 +0530

    nnn

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,10 @@
 def getEnvVar(String paramName){
 
  //return sh (script: "grep '${paramName}' ENV_VARS/project.properties|cut -d'=' -f2", returnStdout: true).trim();
- //return bat (script: "grep '${paramName}' ENV_VARS/project.properties|cut -d'=' -f2", returnStdout: true).trim();
+ return bat (script: "grep '${paramName}' ENV_VARS/project.properties|cut -d'=' -f2", returnStdout: true).trim();
  
  
- return bat (script: 'cd C:\\Program Files\\Git && .\\git-bash.exe echo "hello" ', returnStdout: true).trim();
+ //return bat (script: 'cd C:\\Program Files\\Git && .\\git-bash.exe echo "hello" ', returnStdout: true).trim();
  
  //return bat label: '', script: ''' 
 //cd C:\\Program Files\\Git && .\\git-bash.exe 

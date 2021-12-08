e3b507738eac96dc512e2b34a34c785093e404dd
Prashanth Reddy Billa <prashanth.billa@valuelabs.com>
2021-12-08 11:11:25 +0530

    nnn

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,7 +22,8 @@ agent {
         	steps{
             
         		    script{	
-                    C:\Program Files\Git 	git-bash.exe
+                    //call C:\Program Files\Git 	git-bash.exe
+                    bat label: '', script: 'cd C:\\Program Files \\Git && .\\git.bash.exe  '
                     env.JCLASS_NAME = getEnvVar('JCLASS_NAME')		
 				    env.JCLASS_VERSION = getEnvVar('JCLASS_VERSION')
                     env.JCLASS_SUBVERSION = getEnvVar('JCLASS_SUBVERSION')
